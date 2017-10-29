978f702046094b2c67a4059818a3ee2da14c282b
Wahyu Maruti Adjie <32800189+wmaruti@users.noreply.github.com>
2017-10-29 10:16:13 -0700

    Update padang_kelompokciek.py

--- padang_kelompokciek.py
+++ padang_kelompokciek.py
@@ -74,28 +74,91 @@ Surau Gadang, Nanggalo, Kota Padang, Sumatera Barat',
 
 folium.Marker(
     location=[-0.9920046,100.3807497],
-    popup='Pasar Gaung
-Teluk Bayur, South Padang, Padang City, West Sumatra', 
+    popup='Pasar Gaung. Teluk Bayur, South Padang, Padang City, West Sumatra', 
     icon=folium.Icon(icon='info-sign')
 ).add_to(m)
 
 folium.Marker(
     location=[-0.7724924,100.6402682],
-    popup='Solok,
-    Kota Padang, Sumatera Barat',
+    popup='Solok,Kota Padang, Sumatera Barat',
     icon=folium.Icon(icon='info-sign')
 ).add_to(m)
 
 folium.Marker(
     location=[-0.7724924,100.6402682],
-    popup='Danau Singkarak,
-    X Koto Singkarak, Solok, Sumatera Barat',
+    popup='Danau Singkarak,X Koto Singkarak, Solok, Sumatera Barat',
     icon=folium.Icon(icon='info-sign')
 ).add_to(m)
 
 folium.Marker(
     location=[-0.680632, 100.528263],
-    popup='Puncak Gagoan, Muara Pingai
-   Junjung Sirih, Solok, Sumatera Barat',
+    popup='Puncak Gagoan, Muara Pingai. Junjung Sirih, Solok, Sumatera Barat',
     icon=folium.Icon(icon='info-sign')
 ).add_to(m)
+
+folium.Marker(
+    location=[-0.939662, 100.469540],
+    popup='Limau Manis Sel.',
+    icon=folium.Icon(icon='info-sign')
+).add_to(m)
+
+folium.Marker(
+    location=[-0.942580, 100.472115],
+    popup='Indarung',
+    icon=folium.Icon(icon='info-sign')
+).add_to(m)
+
+folium.Marker(
+    location=[-0.952020, 100.461730],
+    popup='Padang Besi',
+    icon=folium.Icon(icon='info-sign')
+).add_to(m)
+
+folium.Marker(
+    location=[-0.931423, 100.461386],
+    popup='Kapala Koto',
+    icon=folium.Icon(icon='info-sign')
+).add_to(m)
+
+folium.Marker(
+    location=[-0.946356, 100.442160],
+    popup='Bandar Buat',
+    icon=folium.Icon(icon='info-sign')
+).add_to(m)
+
+folium.Marker(
+    location=[-0.962318, 100.440959],
+    popup='Tarantang',
+    icon=folium.Icon(icon='info-sign')
+).add_to(m)
+
+folium.Marker(
+    location=[-0.959229, 100.431002],
+    popup='Koto Lalang',
+    icon=folium.Icon(icon='info-sign')
+).add_to(m)
+
+folium.Marker(
+    location=[-0.962661, 100.423793],
+    popup='Kp. Jua Nan XX',
+    icon=folium.Icon(icon='info-sign')
+).add_to(m)
+
+folium.Marker(
+    location=[-0.941550, 100.424136],
+    popup='Piai Tangah',
+    icon=folium.Icon(icon='info-sign')
+).add_to(m)
+
+folium.Marker(
+    location=[-0.915976, 100.450400],
+    popup='Jl. Batu Busuak',
+    icon=folium.Icon(icon='info-sign')
+).add_to(m)
+
+folium.Marker(
+    location=[-0.927991, 100.433234],
+    popup='Cupak Tangah',
+    icon=folium.Icon(icon='info-sign')
+).add_to(m)
+
